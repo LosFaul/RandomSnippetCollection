f44deb4aff6af1b42e8a75ffe96bc311dad75036
LosFaul <losfaul@gmail.com>
2022-06-10 19:37:31 +0200

    Update Filesystem.h

--- C++/Common/Filesystem.h
+++ C++/Common/Filesystem.h
@@ -9,6 +9,10 @@ namespace Filesystem {
     // simple read text function
     std::string readText(CString filePath);
   
-  
+    bool readBinary(CString filePath, std::vector<char>& buffer);
+
+    template<typename T>
+    bool readBinaryT(CString filePath, std::vector<T>& buffer);
+ 
   
 }
