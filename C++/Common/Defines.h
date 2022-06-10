938a28b496605a970cf4c3176dcf78d5cc452066
LosFaul <losfaul@gmail.com>
2022-06-10 19:04:03 +0200

    Create Defines.h

---
+++ C++/Common/Defines.h
@@ -0,0 +1,59 @@
+#include <cstdint>
+
+
+// ================================================================
+// Integers & Floating Point
+// ================================================================
+
+// Signed Integer 8 Bit
+using i8 = std::int8_t;
+// Unsigned Integer 8 Bit
+using u8 = std::uint8_t;
+
+// Signed Integer 16 Bit
+using i16 = std::int16_t;
+// Unsigned Integer 16 Bit
+using u16 = std::uint16_t;
+
+// Signed Integer 32 Bit
+using i32 = std::int32_t;
+// Unsigned Integer 32 Bit
+using u32 = std::uint32_t;
+
+// Signed Integer 64 Bit
+using i64 = std::int64_t;
+// Unsigned Integer 64 Bit
+using u64 = std::uint64_t;
+
+// Floating Point 32 Bit
+using f32 = float;
+// Floating Point 64 Bit
+using f64 = double;
+
+
+
+// ================================================================
+// Strings
+// ================================================================
+
+using CString = const char*;
+
+	
+
+	
+	
+
+	
+	
+	
+	
+
+	
+	
+	
+	
+
+	
+	
+	
+	
