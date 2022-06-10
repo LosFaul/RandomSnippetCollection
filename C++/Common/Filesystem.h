bcf2f913f9a54e5b08fcbc64cb77a5f2a4b28a7e
LosFaul <losfaul@gmail.com>
2022-06-10 19:31:20 +0200

    Create Filesystem.h

---
+++ C++/Common/Filesystem.h
@@ -0,0 +1,14 @@
+#include "Defines.h"
+
+// includes STL
+#include <string>
+
+
+namespace Filesystem {
+  
+    // simple read text function
+    std::string readText(CString filePath);
+  
+  
+  
+}
