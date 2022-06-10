19d30278309dafa217c7385580b178d697cb1477
LosFaul <losfaul@gmail.com>
2022-06-10 19:38:32 +0200

    Update Filesystem.cpp

--- C++/Common/Filesystem.cpp
+++ C++/Common/Filesystem.cpp
@@ -16,5 +16,45 @@ namespace Filesystem {
     }
   
   
+  	bool readBinary(CString filePath, std::vector<char>& buffer) {
+		std::ifstream file(filePath, std::ios::binary | std::ios::in | std::ios::ate);
+
+
+		if (file.is_open()) { // !file.fail()
+			size_t size = file.tellg();
+			file.seekg(0, std::ios::beg);
+
+			buffer.resize(size);
+
+			file.read((char*)buffer.data(), size);
+			file.close();
+
+			return true;
+		}
+
+		return false;
+	}
+
+
+	template<typename T>
+	bool readBinaryT(CString filePath, std::vector<T>& buffer) {
+		std::ifstream file(filePath, std::ios::binary | std::ios::in | std::ios::ate);
+
+
+		if (file.is_open()) { // !file.fail()
+			size_t size = file.tellg();
+			file.seekg(0, std::ios::beg);
+
+			buffer.resize(size / sizeof(T));
+
+			file.read((char*)buffer.data(), size);
+			file.close();
+
+			return true;
+		}
+
+		return false;
+	}
+
   
 }
